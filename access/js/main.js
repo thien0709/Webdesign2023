74c0142907be31a6648a71a20416812306d03825
thien0709 <thien070904@gmail.com>
2023-04-09 15:05:13 +0700

    add item

--- access/js/main.js
+++ access/js/main.js
@@ -9,10 +9,10 @@ const note = document.querySelector("#content #note");
 const createDel = document.querySelector("#note .head .delete");
 const noti = document.querySelector("#note .head .delete h6");
 const iconNoti = document.querySelector("#note .head .delete i");
+let cr = "fa-solid fa-plus";
+let del = "fa-solid fa-trash";
 newNote.addEventListener("click", function () {
   note.style.display = "block";
-  let cr = "fa-solid fa-plus";
-  let del = "fa-solid fa-trash";
   iconNoti.setAttribute("class", cr);
   noti.innerHTML = "Create";
 });
@@ -21,10 +21,13 @@ const text = document.querySelector("#note .text p");
 const outText = document.querySelector("#content .notes p");
 const notes = document.querySelector("#content #status .notes");
 const tittle = document.querySelector("#note .head h3");
+const h5 = document.querySelector("#note .head .name h5");
+count= 1;
 createDel.addEventListener("click", function () {
   let contents = {
-    // author:,
+    id : count++,
     content: text.textContent,
+    // author : 
   };
   localStorage.setItem("contents", JSON.stringify(contents));
   const x = JSON.parse(localStorage.getItem("contents"));
@@ -32,16 +35,16 @@ createDel.addEventListener("click", function () {
   newDiv.classList.add("div");
   notes.appendChild(newDiv);
   let newH1 = document.createElement("h1");
-  newH1.innerText = `${tittle.textContent}`; 
+  newH1.innerText = `${tittle.textContent}`;
   newDiv.appendChild(newH1);
   var newp = document.createElement("p");
   newp.innerText = `${contents.content}`;
   newDiv.appendChild(newp);
+  //Update by
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  h5.innerHTML = `update at ${hours} : ${minutes} : ${seconds} by `;
 });
-const arrText = [
-  {
-    id: 1,
-    author: "thien123",
-    content: "",
-  },
-];
+//Update by
