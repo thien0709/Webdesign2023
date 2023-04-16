88547b025ba22bbe3f429e750c60ccf5f0810a8e
thien0709 <thien070904@gmail.com>
2023-04-16 10:19:55 +0700

    add client

--- access/js/index.js
+++ access/js/index.js
@@ -2,7 +2,7 @@
 const btnLogin = document.querySelector("#main")
 btnLogin.addEventListener("click",
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
+  // var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
